f44c594b76c2524f160c0e5f7f892338d99081b0
Beatrice Mendoza <mendoza@nyu.edu>
2018-07-09 03:38:17 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,3 +14,9 @@ end
 def move(board, index, character="X")
   board[index] = character
 end
+
+def ask_user_for_move
+  puts "Where would you like to go?"
+  input = gets.strip
+  index = input_to_index(input)
+end
